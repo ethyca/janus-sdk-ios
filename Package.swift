843db92afda4f93fde60f172776bf9bf420a08bd
Jason Gill <103820+gilluminate@users.noreply.github.com>
2026-05-18 11:33:18 -0600

    Release version 1.0.26

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "JanusSDK",
-      url: "https://raw.githubusercontent.com/ethyca/janus-sdk-ios/1.0.25/JanusSDK.xcframework.zip",
-      checksum: "869ba29fbca47db41dd3b2dd1bdd685a780b1a004d5708bc6fa32a0fce4070a8")
+      url: "https://raw.githubusercontent.com/ethyca/janus-sdk-ios/1.0.26/JanusSDK.xcframework.zip",
+      checksum: "07f6a7d9cb64ffcc6c1d8b3a75fb9d2da70833c71027e49a7e83c22fff0179a2")
   ]
 ) 
